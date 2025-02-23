df20b1021fd530e8c588f4282a3e2a6f11dd76ac
praveen kumar siraparapu <121078644+praveen6911@users.noreply.github.com>
2025-02-23 10:46:28 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  host: '0.0.0.0',
+    port: 5173, // Adjust if needed
+    allowedHosts: ['emotorad-assign-2.onrender.com'], 
 })
